471569c4494aaf21fdd53fbe3157caf3e5cc7821
Joachim Kaufmann <joachim_kaufmann@bluewin.ch>
2022-12-23 23:30:35 +0100

    added easier split navigation

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -73,3 +73,11 @@ vim.keymap.set('n', '<A-k>', ":move .-2<CR>==")
 vim.keymap.set('v', '<A-j>', ":move '>+1<CR>gv=gv")
 vim.keymap.set('v', '<A-k>', ":move '<-2<CR>gv=gv")
 
+--
+-- Easier split navigation
+--
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
