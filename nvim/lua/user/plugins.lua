221642b7d689a2d081751463ddf4a85e0b5fe1fc
Joachim Kaufmann <joachim.kaufmann@kanti-baden.ch>
2023-02-20 12:29:39 +0100

    added plugin vim-test

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -13,15 +13,15 @@ local packer_bootstrap = ensure_packer()
 
 require('packer').reset()
 require('packer').init({
-    compile_path = vim.fn.stdpath('data') .. '/site/plugin/packer_compiled.lua',
-    display = {
-        open_fn = function()
-            return require('packer.util').float({
-                border = 'solid'
-            })
-        end
-    }
-})
+        compile_path = vim.fn.stdpath('data') .. '/site/plugin/packer_compiled.lua',
+        display = {
+            open_fn = function()
+                return require('packer.util').float({
+                        border = 'solid'
+                    })
+            end
+        }
+    })
 
 local use = require('packer').use
 
@@ -30,44 +30,44 @@ use('wbthomason/packer.nvim')
 
 -- One Dark theme
 use({
-    'navarasu/onedark.nvim',
-    config = function()
-        vim.cmd('colorscheme onedark')
-        vim.cmd('highlight SpellBad guifg=NONE')
+        'navarasu/onedark.nvim',
+        config = function()
+            vim.cmd('colorscheme onedark')
+            vim.cmd('highlight SpellBad guifg=NONE')
 
-        -- -- Hide the characters in FloatBorder
-        -- vim.api.nvim_set_hl(0, 'FloatBorder', {
-        --   fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
-        --   bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
-        -- })
+            -- -- Hide the characters in FloatBorder
+            -- vim.api.nvim_set_hl(0, 'FloatBorder', {
+            --   fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+            --   bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+            -- })
 
-        -- -- Make the StatusLineNonText background the same as StatusLine
-        vim.api.nvim_set_hl(0, 'StatusLineNonText', {
-            fg = vim.api.nvim_get_hl_by_name('NonText', true).foreground,
-            bg = vim.api.nvim_get_hl_by_name('StatusLine', true).background
-        })
+            -- -- Make the StatusLineNonText background the same as StatusLine
+            vim.api.nvim_set_hl(0, 'StatusLineNonText', {
+                    fg = vim.api.nvim_get_hl_by_name('NonText', true).foreground,
+                    bg = vim.api.nvim_get_hl_by_name('StatusLine', true).background
+                })
 
-        -- -- Hide the characters in CursorLineBg
-        -- vim.api.nvim_set_hl(0, 'CursorLineBg', {
-        --   fg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
-        --   bg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
-        -- })
+            -- -- Hide the characters in CursorLineBg
+            -- vim.api.nvim_set_hl(0, 'CursorLineBg', {
+            --   fg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+            --   bg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+            -- })
 
-        -- vim.api.nvim_set_hl(0, 'NvimTreeIndentMarker', { fg = '#30323E' })
-        vim.api.nvim_set_hl(0, 'IndentBlanklineChar', {
-            fg = '#2F313C'
-        })
+            -- vim.api.nvim_set_hl(0, 'NvimTreeIndentMarker', { fg = '#30323E' })
+            vim.api.nvim_set_hl(0, 'IndentBlanklineChar', {
+                    fg = '#2F313C'
+                })
 
-    end
-})
+        end
+    })
 
 -- Automatically add closing brackets, quotes, etc.
 use({
-    'windwp/nvim-autopairs',
-    config = function()
-        require('nvim-autopairs').setup()
-    end
-})
+        'windwp/nvim-autopairs',
+        config = function()
+            require('nvim-autopairs').setup()
+        end
+    })
 
 -- Commenting support
 use('tpope/vim-commentary')
@@ -98,185 +98,193 @@ use('jessarcher/vim-heritage')
 
 -- Text objects for HTML attributes
 use({
-    'whatyouhide/vim-textobj-xmlattr',
-    requires = 'kana/vim-textobj-user'
-})
+        'whatyouhide/vim-textobj-xmlattr',
+        requires = 'kana/vim-textobj-user'
+    })
 
 -- Automatically set the working directory to the project root
 use({
-    'airblade/vim-rooter',
-    setup = function()
-        -- Instead of this running every time we open a file, we'll just run it once when Vim starts
-        vim.g.rooter_manual_only = 1
-    end,
-    config = function()
-        vim.cmd('Rooter')
-    end
-})
+        'airblade/vim-rooter',
+        setup = function()
+            -- Instead of this running every time we open a file, we'll just run it once when Vim starts
+            vim.g.rooter_manual_only = 1
+        end,
+        config = function()
+            vim.cmd('Rooter')
+        end
+    })
 
 -- Add smooth scrolling to avoid jarring jumps
 use({
-    'karb94/neoscroll.nvim',
-    config = function()
-        require('neoscroll').setup()
-    end
-})
+        'karb94/neoscroll.nvim',
+        config = function()
+            require('neoscroll').setup()
+        end
+    })
 
 -- Split arrays and methods onto multiple lines, or join them back up
 use({
-    'AndrewRadev/splitjoin.vim',
-    config = function()
-        vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
-        vim.g.splitjoin_trailing_comma = 1
-        vim.g.splitjoin_php_method_chain_full = 1
-    end
-})
+        'AndrewRadev/splitjoin.vim',
+        config = function()
+            vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
+            vim.g.splitjoin_trailing_comma = 1
+            vim.g.splitjoin_php_method_chain_full = 1
+        end
+    })
 
 -- Automatically fix identation when pasting code
 use('sickill/vim-pasta')
 
 -- File tree sidebar
 use({
-    'kyazdani42/nvim-tree.lua',
-    requires = 'kyazdani42/nvim-web-devicons',
-    config = function()
-        require('user.plugins.nvim-tree')
-    end
-})
+        'kyazdani42/nvim-tree.lua',
+        requires = 'kyazdani42/nvim-web-devicons',
+        config = function()
+            require('user.plugins.nvim-tree')
+        end
+    })
 
 -- Fuzzy finder
 use({
-    'nvim-telescope/telescope.nvim',
-    requires = {{'nvim-lua/plenary.nvim'}, {'kyazdani42/nvim-web-devicons'}, {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        run = 'make'
-    }, {'nvim-telescope/telescope-live-grep-args.nvim'}},
-    config = function()
-        require('user.plugins.telescope')
-    end
-})
+        'nvim-telescope/telescope.nvim',
+        requires = {{'nvim-lua/plenary.nvim'}, {'kyazdani42/nvim-web-devicons'}, {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                run = 'make'
+        }, {'nvim-telescope/telescope-live-grep-args.nvim'}},
+        config = function()
+            require('user.plugins.telescope')
+        end
+    })
 
 -- A Status line
 use({
-    'nvim-lualine/lualine.nvim',
-    requires = 'kyazdani42/nvim-web-devicons',
-    config = function()
-        require('user.plugins.lualine')
-    end
-})
+        'nvim-lualine/lualine.nvim',
+        requires = 'kyazdani42/nvim-web-devicons',
+        config = function()
+            require('user.plugins.lualine')
+        end
+    })
 
 -- Displays buffers as tabs
 use({
-    'akinsho/bufferline.nvim',
-    requires = 'kyazdani42/nvim-web-devicons',
-    after = 'onedark.nvim',
-    config = function()
-        require('user.plugins.bufferline')
-    end
-})
+        'akinsho/bufferline.nvim',
+        requires = 'kyazdani42/nvim-web-devicons',
+        after = 'onedark.nvim',
+        config = function()
+            require('user.plugins.bufferline')
+        end
+    })
 
 -- Display identation line
 use({
-    'lukas-reineke/indent-blankline.nvim',
-    config = function()
-        require('user.plugins.indent-blankline')
-    end
-})
+        'lukas-reineke/indent-blankline.nvim',
+        config = function()
+            require('user.plugins.indent-blankline')
+        end
+    })
 
 -- Add a dashboard
 use({
-    'glepnir/dashboard-nvim',
-    event = 'VimEnter',
-    config = function()
-        require('user.plugins.dashboard')
-    end,
-    requires = {'nvim-tree/nvim-web-devicons'}
-})
+        'glepnir/dashboard-nvim',
+        event = 'VimEnter',
+        config = function()
+            require('user.plugins.dashboard')
+        end,
+        requires = {'nvim-tree/nvim-web-devicons'}
+    })
 
 -- Git integration
 use({
-    'lewis6991/gitsigns.nvim',
-    requires = 'nvim-lua/plenary.nvim',
-    config = function()
-        require('gitsigns').setup()
-        vim.keymap.set('n', ']h', ':Gitsigns next_hunk<CR>')
-        vim.keymap.set('n', '[h', ':Gitsigns prev_hunk<CR>')
-        vim.keymap.set('n', 'gs', ':Gitsigns stage_hunk<CR>')
-        vim.keymap.set('n', 'gS', ':Gitsigns undo_stage_hunk<CR>')
-        vim.keymap.set('n', 'gp', ':Gitsigns preview_hunk<CR>')
-        vim.keymap.set('n', 'gb', ':Gitsigns blame_line<CR>')
-    end
-})
+        'lewis6991/gitsigns.nvim',
+        requires = 'nvim-lua/plenary.nvim',
+        config = function()
+            require('gitsigns').setup()
+            vim.keymap.set('n', ']h', ':Gitsigns next_hunk<CR>')
+            vim.keymap.set('n', '[h', ':Gitsigns prev_hunk<CR>')
+            vim.keymap.set('n', 'gs', ':Gitsigns stage_hunk<CR>')
+            vim.keymap.set('n', 'gS', ':Gitsigns undo_stage_hunk<CR>')
+            vim.keymap.set('n', 'gp', ':Gitsigns preview_hunk<CR>')
+            vim.keymap.set('n', 'gb', ':Gitsigns blame_line<CR>')
+        end
+    })
 
 use({
-    'tpope/vim-fugitive',
-    requires = 'tpope/vim-rhubarb'
-})
+        'tpope/vim-fugitive',
+        requires = 'tpope/vim-rhubarb'
+    })
 
 use({
-    'voldikss/vim-floaterm',
-    config = function()
-        require('user.plugins.floaterm')
-    end
-})
+        'voldikss/vim-floaterm',
+        config = function()
+            require('user.plugins.floaterm')
+        end
+    })
 
 -- Improved syntax highlighting and other things
 use({
-  'nvim-treesitter/nvim-treesitter',
-  run = function()
-      require('nvim-treesitter.install').update({with_sync = true})
-  end,
-  requires = {
-    'nvim-treesitter/playground',
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    'JoosepAlviste/nvim-ts-context-commentstring',
-  },
-  config = function()
-    require('user.plugins.treesitter')
-  end,
-})
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            require('nvim-treesitter.install').update({with_sync = true})
+        end,
+        requires = {
+            'nvim-treesitter/playground',
+            'nvim-treesitter/nvim-treesitter-textobjects',
+            'JoosepAlviste/nvim-ts-context-commentstring',
+        },
+        config = function()
+            require('user.plugins.treesitter')
+        end,
+    })
 
 -- Language Server Protocol
 use({
-  'neovim/nvim-lspconfig',
-  requires = {
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    'jose-elias-alvarez/null-ls.nvim',
-    'jayp0521/mason-null-ls.nvim',
-  },
-  config = function()
-    require('user.plugins.lspconfig')
-  end,
-})
+        'neovim/nvim-lspconfig',
+        requires = {
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
+            'jose-elias-alvarez/null-ls.nvim',
+            'jayp0521/mason-null-ls.nvim',
+        },
+        config = function()
+            require('user.plugins.lspconfig')
+        end,
+    })
 
 -- Completion
 use({
-  'hrsh7th/nvim-cmp',
-  requires = {
-    'L3MON4D3/LuaSnip',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-nvim-lsp-signature-help',
-    'hrsh7th/cmp-nvim-lua',
-    'jessarcher/cmp-path',
-    'onsails/lspkind-nvim',
-    'saadparwaiz1/cmp_luasnip',
-  },
-  config = function()
-    require('user.plugins.cmp')
-  end,
-})
+        'hrsh7th/nvim-cmp',
+        requires = {
+            'L3MON4D3/LuaSnip',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lsp-signature-help',
+            'hrsh7th/cmp-nvim-lua',
+            'jessarcher/cmp-path',
+            'onsails/lspkind-nvim',
+            'saadparwaiz1/cmp_luasnip',
+        },
+        config = function()
+            require('user.plugins.cmp')
+        end,
+    })
 
 -- PHP Refactoring Tools
 use({
-  'phpactor/phpactor',
-  ft = 'php',
-  run = 'composer install --no-dev --optimize-autoloader',
-  config = function()
-      vim.keymap.set('n', '<leader>pm', ':PhpactorContextMenu<CR>')
-  end,
-})
+        'phpactor/phpactor',
+        ft = 'php',
+        run = 'composer install --no-dev --optimize-autoloader',
+        config = function()
+            vim.keymap.set('n', '<leader>pm', ':PhpactorContextMenu<CR>')
+        end,
+    })
+
+-- Testing helper
+use({
+        'vim-test/vim-test',
+        config = function()
+            require('user.plugins.vim-test')
+        end,
+    })
 
 -- Automatically set up your configuration after cloning packer.nvim
 -- Put this at the end after all plugins
